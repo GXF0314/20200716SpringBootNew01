4c530e703518165d3fa45127d7ccdba7746fd8f7
GXF0314 <1124101185@qq.com>
2020-06-25 16:45:26 +0800

    黑马第一次把本地工程添加到git

---
+++ Git_test/src/g/x/bean/GitTest.java
@@ -0,0 +1,11 @@
+package g.x.bean;
+
+/**
+ * @auther gxf
+ * @date2020/6/25-16:31
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
